1fef64db435286c5e80fe4540486ba33cf97af85
unknown <SSDD@PNAWSA06LC1007B.duoc.acad>
2024-04-30 12:20:30 -0400

    Final Class

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const obtenerDigimonPromesas = () => {
+    return new Promise((resolve,reject)=>{
+        fetch('https://digimon-api.vercel.app/api/digimon')
+        .then((digimones)=>{
+            if(!digimones.ok){
+                throw new Error("Error");
+            }
+            return digimones.json();
+        })
+        .then ((data)=>{
+            resolve(data);
+        })
+        .catch((error)=>{
+            reject(`El error es: ${error}`);
+        })
+    })
+}
+obtenerDigimonPromesas()
+    .then((digimones)=>{
+        digimones.map((digimon)=>{
+            console.log(digimon);
+        })
+    }).catch((error)=>{
+        console.log(`El error es: ${error}`);
+    })
